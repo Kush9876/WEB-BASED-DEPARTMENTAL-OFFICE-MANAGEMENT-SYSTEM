ec50b4860f3ada38dc0689f649e3982501e90351
Kush9876 <kumarkush400@gmail.com>
2024-12-11 10:25:06 +0530

    Initial commit for WEB BASED DEPARTMENTAL OFFICE MANAGEMENT SYSTEM

---
+++ contact.php
@@ -0,0 +1,31 @@
+<?php include("header.php"); ?>
+<?php include('db_con.php'); ?>
+<div class="container-fluid">
+    <div class="row" id="wrapper">
+        <?php require_once 'menubar.php'; ?>
+        <div class="col-sm-12">
+            <div class="panel panel-default">
+                <div class="panel-body">
+
+                    <center>
+                        <h1>Department of Computer Science and Engineering</h1>
+                    </center>
+                    <hr>
+
+                    <div class="col-sm-4 col-sm-offset-4" style="height:430px;">
+                        <p>Right Block, 1st Building<br>
+                            Tezpur University<br>
+                            Tezpur, Assam.<br>
+                            Cell: +880 747000700<br>
+                            Tel: +880 721 711103<br>
+                            Fax: +880 721 750064, 711251<br>
+                            E-mail: cse@tu.ac.in<br>
+                            URL: www.tu.ac.in/cse<br>
+                        </p>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php include("header.php"); ?>
+
+<div class="container-fluid">
+	<div class="row" id="wrapper">
+		<?php require_once 'menubar.php'; ?>
+		<div class="col-sm-12">
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<center>
+						<h1 style="color:#0B3861; font-family:Impact;">Tezpur University</h1>
+						<h3 style="color:#0B615E; font-family:Elephant;">Department of Computer Science & Engineering</h3>
+						<img class="img-circle" alt="Image Preview" src="pics/logo2.png" />
+					</center>
+				</div>
+			</div>
+			<div class="panel panel-default">
+				<div class="panel-body">
+					<div class="media">
+						<a href="#" class="pull-left"><img alt="Bootstrap Media Preview" src="pics/chairman2.jpg" class="media-object" /></a>
+						<div class="media-body">
+							<h4 class="media-heading" style="font-family:Bodoni MT Condensed; color:#0489B1;">
+								About The Department
+							</h4> The Department of Computer Science and Engineering came into being in 2006 as a result of renaming of the Department of Computer Science and Information Technology. The Department of Computer Science and the Department of Information Technology were merged in 2004 and renamed as Department of Computer Science and Information Technology.
+
+							The Department of Computer Science was started in July, 1994 under the school of Science and Technology. It was started with the object of promoting specialized knowledge in the field of Computer Science and Computer Application. In view of the world wide shortage of skilled manpower in the field of Information Technology, the Department strives to channelize talented youth of the region to this challenging and knowledge intensive field by imparting quality technical knowledge and skills in the field. The programmes started under this department were Master of Computer Application (MCA), Master of Science (by Research) in Computer Science and PhD in Computer Science. The programmes have been designed keeping in mind the needs of the global Information Technology Industry.
+
+							The Department of Information Technology was started in July, 1997 under the school of Science and Technology. The Department was established with the aim to contribute towards research in the field of Information Technology and to fulfill the increasing demand for highly qualified manpower in the field for Industries as well as academic institutions. The programmes started under this department were Master of Technology (M Tech) in Information Technology, and PhD in Information Technology.
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
